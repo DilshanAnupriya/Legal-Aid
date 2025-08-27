5e38b22ab4e8bb4a4288a75038df413c308f7856
Nadeeema <>
2025-08-27 09:04:42 +0530

    Refactor HomeScreen component to replace StackNavigator with HomePageScreen and clean up unused imports

--- Client/app/(tabs)/index.tsx
+++ Client/app/(tabs)/index.tsx
@@ -1,38 +1,24 @@
-import { StyleSheet, View,Text } from 'react-native';
-import {useEffect, useState} from "react";
+import { StyleSheet, View } from 'react-native';
+import { useState } from "react";
 import SplashScreen from "@/components/ui/screen/SplashScreen";
-import StackNavigator from "@/app/navigation/stack-navigation/StackNavigator";
 import HomePageScreen from "@/components/ui/screen/HomePageScreen";
 
-
-
-
 export default function HomeScreen() {
-
   const [isLoading, setIsLoading] = useState(true);
 
-
-
   return (
-      <View style={styles.container}>
-        {isLoading ? (
-            <SplashScreen onFinish={()=>{setIsLoading(false)}}/>
-        ) : (
-            <StackNavigator/>
-        )}
-      </View>
+    <View style={styles.container}>
+      {isLoading ? (
+        <SplashScreen onFinish={() => { setIsLoading(false) }} />
+      ) : (
+        <HomePageScreen />
+      )}
+    </View>
   );
 }
 
-
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
-  },
-  text: {
-    fontSize: 18,
-    color: "blue",
-    fontWeight: "600",
   }
-})
+});
---
+++ Client/app/(tabs)/profile.tsx
@@ -0,0 +1,5 @@
+import ProfileScreen from "@/components/ui/screen/ProfileScreen";
+
+export default function Profile() {
+  return <ProfileScreen />;
+}
