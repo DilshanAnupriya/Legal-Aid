eb6668d96799c7da37dfea9bce9bb316fd4fe7ba
hirun athukorala <hirun.athukorala@arctiq.com.au>
2025-09-24 18:10:11 +0530

    pckg changes

--- Client/app/(tabs)/index.tsx
+++ Client/app/(tabs)/index.tsx
@@ -1,26 +1,28 @@
 import { StyleSheet, ScrollView, View } from 'react-native';
 import { useState } from "react";
 import SplashScreen from "@/components/ui/screen/SplashScreen";
-import HomePageScreen from "@/components/ui/screen/HomePageScreen";
 import { ThemeProvider } from '@/context/ThemeContext';
-import StackNavigator from "@/app/navigation/stack-navigation/StackNavigator";
-import {AuthProvider} from "@/context/AuthContext";
+import { AuthProvider } from "@/context/AuthContext";
 import AuthNavigator from "@/app/navigation/stack-navigation/AuthStackNavigator";
 
 export default function HomeScreen() {
   const [isLoading, setIsLoading] = useState(true);
+
   return (
-      <AuthProvider>
-    <ThemeProvider>
-      <View style={styles.container}>
-        {isLoading ? (
-            <SplashScreen onFinish={()=>{setIsLoading(false)}}/>
-        ) : (
-            <AuthNavigator/>
-        )}
-      </View>
-    </ThemeProvider>
-      </AuthProvider>
+    <AuthProvider>
+      <ThemeProvider>
+        <ScrollView 
+          style={styles.container} 
+          contentContainerStyle={{ flexGrow: 1 }}
+        >
+          {isLoading ? (
+            <SplashScreen onFinish={() => setIsLoading(false)} />
+          ) : (
+            <AuthNavigator />
+          )}
+        </ScrollView>
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
 
@@ -29,4 +31,3 @@ const styles = StyleSheet.create({
     flex: 1,
   }
 });
-
